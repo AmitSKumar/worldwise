00e791f185b723942ab777249ff85b3c23fd9af9
Amit Singh <amitsinghbxr@gmail.com>
2023-10-16 12:32:28 +0530

    implement routing and creating pages/routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Product from "./Pages/product"
+import Pricing from "./Pages/Pricing"
+import HomePage from "./Pages/HomePage"
+import PageNotFound from "./Pages/PageNotFound"
 function App() {
   return (
     <div>
-      world Wise
+     <BrowserRouter >
+     <Routes>
+     <Route path="/" element={<HomePage/>} />
+      <Route path="product" element={<Product/>} />
+      <Route path="pricing" element={<Pricing/>} />
+      <Route path="*" element={<PageNotFound/>} />
+     </Routes>
+     </BrowserRouter>
     </div>
   )
 }
