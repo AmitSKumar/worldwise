f6b951ca9778d5340f8f071e433f460865d2ad78
Amit Singh <amitsinghbxr@gmail.com>
2023-10-17 22:00:50 +0530

    <Navigate / >   element to redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes,Route, Navigate } from "react-router-dom"
 import Product from "./Pages/product"
 import Pricing from "./Pages/Pricing"
 import HomePage from "./Pages/HomePage"
@@ -41,7 +41,7 @@ function App() {
       <Route path="login" element={<Login/>} />
       <Route path="app" element={<AppLayout/>} >
         {/* index route will shown when no other route matches */}
-        <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+        <Route index element={<Navigate replace to="cities"/>} />
         <Route  path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}/>
         <Route  path="cities/:id" element={<City />}/>
         <Route  path="countries" element={<CountryList cities={cities}  />}/>
