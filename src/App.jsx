fa90dd11e0125147b354913ad2f8146ce2fae6d4
Amit Singh <amitsinghbxr@gmail.com>
2023-10-17 12:39:23 +0530

    nested and index route implementation

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,17 @@ function App() {
     <div>
      <BrowserRouter >
      <Routes>
-     <Route path="/" element={<HomePage/>} />
+     <Route index element={<HomePage/>} />
       <Route path="product" element={<Product/>} />
       <Route path="pricing" element={<Pricing/>} />
       <Route path="login" element={<Login/>} />
-      <Route path="app" element={<AppLayout/>} />
+      <Route path="app" element={<AppLayout/>} >
+        {/* index route will shown when no other route matches */}
+        <Route index element={<p>List</p>} />
+        <Route  path="cities" element={<p>List of Cities</p>}/>
+        <Route  path="countries" element={<p>List of countries</p>}/>
+        <Route  path="form" element={<p>Form</p>}/>
+      </Route>
       <Route path="*" element={<PageNotFound/>} />
      </Routes>
      </BrowserRouter>
